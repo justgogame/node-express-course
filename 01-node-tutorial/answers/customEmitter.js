42484e910daf99b20d7c48b7b4b3d1fac011d31f
justgogame <justnite@gmail.com>
2024-11-18 22:32:26 -0500

    answers for lesson 2

---
+++ 01-node-tutorial/answers/customEmitter.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('response', (name, state, id) => {
+    console.log(`Data recieved: ${name} from ${state}, id ${id}`);
+})
+
+
+
+customEmitter.emit('response', 'John', 'NC', 34)
